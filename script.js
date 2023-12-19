b2f1cc13ed75605d6c75296eec3ec8846c19ede8
Ilya <zenqst@gmail.com>
2023-12-19 17:55:01 +0300

    Big update

--- script.js
+++ script.js
@@ -15,13 +15,12 @@ let heroes = [
 		'Мария',
 		'Практикум',
 		'Лучшая наставница во всём мире, которая вообще супер крутая и лучше некуда вообще',
-		'images/img_indev.svg'
+		'images/not_img.svg'
 	]
 ];
 
 let heroesContainer = document.getElementById('heroesContainer');
 
-// Загрузка данных из локального хранилища
 function loadDataFromLocalStorage() {
 	let storedData = localStorage.getItem('heroesData');
 	if (storedData) {
@@ -30,7 +29,6 @@ function loadDataFromLocalStorage() {
 	}
 }
 
-// Сохранение данных в локальном хранилище
 function saveDataToLocalStorage() {
 	localStorage.setItem('heroesData', JSON.stringify(heroes));
 }
@@ -47,7 +45,9 @@ function displayHeroes() {
 		let heroDescription = heroes[i][2];
 		let heroImgs = heroes[i][3];
 
-		heroDiv.innerHTML = `<img src="${heroImgs}" class="hero-card__image" alt="Картинка персонажа" /><div class='hero-card__text'> <div class='hero-card__text-two'> <h2 class='hero-card__title'>${heroNames}</h2><p class='hero-card__organization'>${heroOrganizations}</p> </div> <p class='hero-card__description'>${heroDescription}</p> </div>`;
+		let imageUrl = heroImgs ? heroImgs : 'images/not_img.svg';
+
+		heroDiv.innerHTML = `<img src="${imageUrl}" class="hero-card__image" alt="Картинка персонажа" /><div class='hero-card__text'> <div class='hero-card__text-two'> <h2 class='hero-card__title'>${heroNames}</h2><p class='hero-card__organization'>${heroOrganizations}</p> </div> <p class='hero-card__description'>${heroDescription}</p> </div>`;
 		heroesContainer.appendChild(heroDiv);
 	}
 }
@@ -83,7 +83,7 @@ function addHero() {
 	let descriptionInput = document.getElementById('heroDescription');
 	let imageInput = document.getElementById('heroImage');
 
-	if (nameInput.value && descriptionInput.value && imageInput.value) {
+	if (nameInput.value && descriptionInput.value) {
 		heroes.push([nameInput.value, organizationInput.value, descriptionInput.value, imageInput.value]);
 		displayHeroes();
 		saveDataToLocalStorage(); // Сохранение данных после добавления
